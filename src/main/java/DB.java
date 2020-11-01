6b18326eb41537f412b8db1053b092edaea32a4e
amodoipeter <amodoipeter@gmail.com>
2020-11-01 06:18:14 +0300

    Added heroku DB

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,8 +1,13 @@
-
-import org.sql2o.*;
+import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild", "moringa", "Ap@33358371");
+//    postgresql-concentric-33639
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild", "moringa", "Ap@33358371");
+//    postgres://jieoxouxjsarmw:aca439265342852420efb13d28330b3bbc5a36eddfb469c0ddf063be93d1c22b@ec2-3-233-236-188.compute-1.amazonaws.com:5432/d7om722o88p9i
+
+    static String connectionString = "jdbc:postgresql://ec2-54-83-33-14.compute-1.amazonaws.com:5432/d7om722o88p9i"; //!
+    static Sql2o sql2o = new Sql2o(connectionString, "jieoxouxjsarmw", "aca439265342852420efb13d28330b3bbc5a36eddfb469c0ddf063be93d1c22b"); //!
+
 }
 
 
