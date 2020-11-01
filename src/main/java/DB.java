58a07f8436d65189f2f6b9c645774ad86893bdcb
amodoipeter <amodoipeter@gmail.com>
2020-11-01 09:54:17 +0300

    commit for deploy to heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,10 +2,10 @@ import org.sql2o.Sql2o;
 
 public class DB{
 //    postgresql-concentric-33639
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild", "moringa", "Ap@33358371");
+//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild", "moringa", "Ap@33358371");
 //    postgres://jieoxouxjsarmw:aca439265342852420efb13d28330b3bbc5a36eddfb469c0ddf063be93d1c22b@ec2-3-233-236-188.compute-1.amazonaws.com:5432/d7om722o88p9i
 
-    public static Sql2o sql2o  = new Sql2o("jdbc:postgresql://ec2-54-83-33-14.compute-1.amazonaws.com:5432/d7om722o88p9i", "jieoxouxjsarmw", "aca439265342852420efb13d28330b3bbc5a36eddfb469c0ddf063be93d1c22b");
+    public static Sql2o sql2o  = new Sql2o("jdbc:postgresql://ec2-3-208-224-152.compute-1.amazonaws.com:5432/d6tclvuu3g9tdh\n", "ujifyjjserbuwf", "d4ff9209243294046fa67653d5bdd6199190bd01685275cce61b55e19f77b46b");
 
 }
 
